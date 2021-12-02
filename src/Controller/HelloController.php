592a9d6130115b3f2d150202a7b79f2e3991bdd0
Thrall81 <florent.barbaouat@gmail.com>
2021-12-02 13:01:11 +0100

    added annotations

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController extends AbstractController
+{
+    /**
+     * @Route("hello")
+     */
+    public function hello(Request $request)
+    {
+        $params = $request->query->all();
+        $string = "Les paramètres sont : </br>";
+        foreach ($params as $key => $value) {
+            $string = $string . '-' . $key . ':' . $value . '</br>';
+        }
+        return new Response($string);
+    }
+}
